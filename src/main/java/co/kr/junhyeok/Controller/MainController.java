8f57a7608299f5d9c533a9aa66b883dc08075fa4
junhyeokkk <loveu9911@naver.com>
2024-10-11 11:27:11 +0900

    first commit

---
+++ src/main/java/co/kr/junhyeok/Controller/MainController.java
@@ -0,0 +1,12 @@
+package co.kr.junhyeok.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "/index";
+    }
+}
---
+++ src/test/java/co/kr/junhyeok/SpringJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.junhyeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
